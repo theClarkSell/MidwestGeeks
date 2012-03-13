5d90f551614e0343602934dd6b06431b4e0a733d
Pat Paasch <ppaasch@gfxi.com>
2012-03-13 17:04:24 -0500

    initial commit with MVC3 and Kendo UI

---
+++ src/MidwestGeeks.Web/Controllers/EventsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MidwestGeeks.Controllers
+{
+    public class EventsController : Controller
+    {
+        //
+        // GET: /Events/
+
+        public ActionResult Index()
+        {
+            dynamic weeklyEvents = new List<dynamic>()
+                                       {
+                                           new { 
+                                               Name = "Chicago Alt.Net",
+                                               Date = new DateTime(2012, 3, 14).ToLongDateString(),
+                                               StartTime = "6:00pm",
+                                               Location = "SPR/Redpoint Technologies",
+                                               Address = "233 South Wacker Dr, 35th Floor",
+                                               City = "Chicago",
+                                               State = "Illinois",
+                                               ZipCode = "60606",
+                                               WebsiteUrl = "www.chicagoalt.net"
+                                           },
+                                           new { 
+                                               Name = "angleBracket",
+                                               Date = new DateTime(2012, 3, 15).ToLongDateString(),
+                                               StartTime = "7:00pm",
+                                               Location = "College of Lake County",
+                                               Address = "19351 West Washington",
+                                               City = "Grayslake",
+                                               State = "Illinois",
+                                               ZipCode = "60030",
+                                               WebsiteUrl = "www.anglebracket.net"
+                                           },
+                                           new { 
+                                               Name = "Lcnug",
+                                               Date = new DateTime(2012, 4, 12).ToLongDateString(),
+                                               StartTime = "7:00pm",
+                                               Location = "College of Lake County",
+                                               Address = "19351 West Washington",
+                                               City = "Grayslake",
+                                               State = "Illinois",
+                                               ZipCode = "60030",
+                                               WebsiteUrl = "www.lcnug.org"
+                                           }
+
+                                       };
+
+            return Json(new {results = weeklyEvents }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ src/MidwestGeeks.Web/Controllers/GroupsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MidwestGeeks.Controllers
+{
+    public class GroupsController : Controller
+    {
+        //
+        // GET: /Groups/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            dynamic groups = new List<dynamic>()
+                                 {
+                                     new {Name = "angleBracket", Pitch = "Super Awesome"},
+                                     new {Name = "Lcnug", Pitch = ".Net Nerds"},
+                                     new {Name = "Chicago Alt.Net", Pitch = "Hipster .Netters"}
+                                 };
+
+            return Json(new {results = groups}, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
