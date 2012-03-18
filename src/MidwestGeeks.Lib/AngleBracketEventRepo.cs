a71a95e0422bb0b7c5c03cd1301ce4a691b5dd9f
unknown <ppaasch@.(none)>
2012-03-17 23:35:16 -0500

    UI stuff to add previous and next weeks (no backend support) Looked at EventBrite api

---
+++ src/MidwestGeeks.Lib/AngleBracketEventRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace MidwestGeeks.Lib
+{
+    public class AngleBracketEventRepo
+    {
+        //playing with Eventbrite Api
+        public String ListEvents()
+        { 
+            var appKey = "SGX6PYXNUOXEGYPQJJ"; //my eventbrite app key
+            var angleBracketId = "1647971612";
+            var api_url = "https://www.eventbrite.com/json/organizer_list_events?app_key=" + appKey + "&id=" + angleBracketId;
+
+            var request = WebRequest.Create(api_url);
+            var response = request.GetResponse();
+
+            var stream = response.GetResponseStream();
+            var reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
+        }
+
+    }
+}
--- src/MidwestGeeks.Tests/NoTests.cs
+++ src/MidwestGeeks.Tests/NoTests.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using MidwestGeeks.Lib;
 
 namespace MidwestGeeks.Tests
 {
+    [TestFixture]
     public class NoTests
     {
+        [Test]
+        public void EventBriteApiTest()
+        {
+            var angleBracketEvents = new AngleBracketEventRepo();
+
+            var results = angleBracketEvents.ListEvents();
+
+            Assert.IsNotEmpty(results);
+        }
     }
 }
